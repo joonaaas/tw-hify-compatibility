13d9a80d4f08a0ddd1dfdc084a5b54ef96f85d44
Jonas Alolino <jonasqalolino@gmail.com>
2021-02-28 17:53:23 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'pop': ['Poppins', 'sans-serif'],
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
